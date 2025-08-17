de682012b670784a4758f4cb63659da231ccbc5b
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2025-08-17 15:24:03 +0530

    Update db.js

--- Frontend/config/db.js
+++ Frontend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/testdb", {
+    await mongoose.connect("mongodb+srv://riturajrai2020:Riturajrai%409955@cluster0.zdhjjed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -14,3 +14,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
